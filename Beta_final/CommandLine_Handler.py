3eac5ed18a3ceffc3d4f71ae6c09efedaa68d7db
DESKTOP-8VEJUR0\CEOS <nchrisr@myumanitoba.ca>
2018-07-30 11:32:30 -0500

    Cleaned up CommandLineHandler, added more logs and comments.

--- Beta_final/CommandLine_Handler.py
+++ Beta_final/CommandLine_Handler.py
@@ -56,23 +56,28 @@ def handle_file_input(input_file, template_path, template_map_path, formats, dir
     # Move the processing files into the right directories
     loaded_data_path_obj = pathlib2.Path(loaded_data_path)
     loaded_data_parent = loaded_data_path_obj.parent
+    print("Making directory for Loaded data template. ")
     loaded_data_destination =(loaded_data_parent.joinpath(directories["outputs"])).joinpath(directories['loaded'])
+    print("Moving loaded data file into new directory")
     if ((loaded_data_destination.joinpath(loaded_data_path_obj.name)).exists()):
-        print("A file with name {} already exists in the destination path provided to move."
+        print("\nA file with name {} already exists in the destination path provided to move."
               .format(loaded_data_path_obj.name))
         response=raw_input("Would you like to replace the file with the newly processed one? (Y/N)")
         if response.lower() == 'y':
             os.remove(str(loaded_data_destination.joinpath(loaded_data_path_obj.name)))
+
             shutil.move(loaded_data_path, str(loaded_data_destination))
     else:
         shutil.move(loaded_data_path, str(loaded_data_destination))
 
     normalized_path_obj = pathlib2.Path(normalized_path)
     normalized_path_parent = normalized_path_obj.parent
+    print("Making directory for Normalized data template. ")
     normalized_data_destination = (normalized_path_parent.joinpath(directories["outputs"])).\
         joinpath(directories['normalized'])
+    print("Moving Normalized data file into new directory.")
     if((normalized_data_destination.joinpath(normalized_path_obj.name)).exists()):
-        print("A file with name {} already exists in the destination path provided to move."
+        print("\nA file with name {} already exists in the destination path provided to move."
               .format(normalized_path_obj.name))
         response=raw_input("Would you like to replace the file with the newly processed one? (Y/N)")
         if response.lower() == 'y':
@@ -85,9 +90,11 @@ def handle_file_input(input_file, template_path, template_map_path, formats, dir
     if move_ip_map:
         ip_map = pathlib2.Path(input_map_path)
         ip_parent = ip_map.parent
+        print("Making directory for json maps. ")
         json_templates_path = ip_parent.joinpath(directories['json_maps'])
+        print("Moving json maps data file into new directory.")
         if((json_templates_path.joinpath(ip_map.name)).exists()):
-            print("A file with name {} already exists in the destination path provided to move, move not possible."
+            print("\nA file with name {} already exists in the destination path provided to move, move not possible."
                   .format(ip_map.name))
             response = raw_input("Would you like to replace the file with the newly processed one? (Y/N)")
             if response.lower() == 'y':
@@ -115,6 +122,7 @@ def handle_dir_input(dir_path, template_path, template_map_path, formats, direct
     # Do this if all the files in the directory have the same format.
     if (same_file_formats):
         # Check to see if the input file map was provided, if not ask questions to generate it.
+        print("Processing files with all same formats")
         if(input_map_path==None or input_map_path==''):
             input_path = pathlib2.Path(files_list[0])
             parent_dir = pathlib2.Path(input_path.parent)
@@ -131,16 +139,18 @@ def handle_dir_input(dir_path, template_path, template_map_path, formats, direct
                                   input_map_path=input_map_path,
                                   move_ip_map=False)
             except Exception as e:
+                traceback.print_exc()
                 print("Processing {} failed".format(file))
                 print(e)
-                traceback.print_exc()
 
         # Move the files into the right directories after processing.
         ip_map = pathlib2.Path(input_map_path)
         ip_parent = ip_map.parent
+        print("Making directory for json maps. ")
         json_templates_path = ip_parent.joinpath(directories['json_maps'])
+        print("Moving json maps data file into new directory.")
         if (json_templates_path.joinpath(ip_map.name)).exists():
-            print("A file with name {} already exists in the destination path provided to move."
+            print("\nA file with name {} already exists in the destination path provided to move."
                   .format(ip_map.name))
             response = raw_input("Would you like to replace the file with the newly processed one? (Y/N)")
             if response.lower() == 'y':
@@ -150,9 +160,10 @@ def handle_dir_input(dir_path, template_path, template_map_path, formats, direct
             shutil.move(str(ip_map), str(json_templates_path))
 
     else:
+        print("Processing files with same formats")
         # If the files to be loaded are not all in the same format, load them this way
         for file in files_list:
-            print("Fill in data location information for {}".format(pathlib2.Path(file).name))
+            print("\nFill in data location information for {}".format(pathlib2.Path(file).name))
             try:
                 handle_file_input(file, template_path, template_map_path, formats, directories)
             except Exception as e:
@@ -188,7 +199,8 @@ def main():
         print("At least 2 arguments are required for the scripts to run successfully.")
         exit(-1)
     else:
-        print("Complete args...")
+        print("Complete args... ")
+        print("Parsing arguments... ")
         args_dictionary = parse_arguments(arguments)
         input_file_or_dir = args_dictionary['input']
         template_file_path = args_dictionary['template']
@@ -225,7 +237,9 @@ def main():
     # Create path object for the input data,call methods to process and load the data into the templates
     input_data_path = pathlib2.Path(input_file_or_dir)
     if (input_data_path.exists()):
+        # If the input path is a file, process it like this.
         if (input_data_path.is_file()):
+            print('Processing single file.')
             input_fname=input_data_path.name
             input_fname_split=input_fname.split('.')
             extension=input_fname_split[-1]
@@ -244,7 +258,10 @@ def main():
                 print("Failed to process {}".format(input_file_or_dir))
                 print(e)
                 traceback.print_exc()
+
+        # If the input path is a directory, process it like this.
         elif(input_data_path.is_dir()):
+            print("Processing a directory of files.")
             json_maps_dir = input_data_path.joinpath(dirs["json_maps"])
             if (not (json_maps_dir.exists())):
                 os.mkdir(str(json_maps_dir))
