1294e61d9573bc873d11240d84fed44b956e9bad
DESKTOP-8VEJUR0\CEOS <nchrisr@myumanitoba.ca>
2019-03-01 12:59:39 -0600

    -> Changed some lines of code in DataLoader.py that didn't match pep8 -> Added more documentation to DataLoader.py

--- Beta_final/Data_Loader.py
+++ Beta_final/Data_Loader.py
@@ -40,19 +40,19 @@ def handle_csv_input(filename, input_specs):
         map_dict = json.load(j_maps)
 
     # Get the specification for which row contains the headings in the file
-    data = None
-    file_header_column = int(map_dict['header_row'])
+    data_frame = None  # This will be the data_frame containing the data from 'filename'
+    file_header_row = int(map_dict['header_row'])
     # Decide how to read in the file based on the header specification.
-    if file_header_column <= NO_HEADER_ROW_INDICATOR:
-        data = pd.read_csv(filename, header=None)
-    elif file_header_column == 1:
-        data = pd.read_csv(filename)
-    elif file_header_column > 1:
+    if file_header_row <= NO_HEADER_ROW_INDICATOR:
+        data_frame = pd.read_csv(filename, header=None)
+    elif file_header_row == DEFAULT_HEADER_ROW_NUMBER:
+        data_frame = pd.read_csv(filename)
+    elif file_header_row > DEFAULT_HEADER_ROW_NUMBER:
         # If the header row is not the first, do this
-        data = pd.read_csv(filename, header=file_header_column-1)
+        data_frame = pd.read_csv(filename, header=file_header_row-1)
 
     # Get the number of rows and columns and generate a list containing default header names.
-    rows, cols = data.shape
+    rows, cols = data_frame.shape
     header_names = [DEFAULT_UNKNOWN]*cols
 
     # Change the default header names to the actual header names as specified in the json specification data.
@@ -66,7 +66,7 @@ def handle_csv_input(filename, input_specs):
                   format(key, value))
         else:
             header_names[int(value)-1] = key
-    data.columns = header_names
+    data_frame.columns = header_names
 
     # Check to see if formats are present and generate a boolean value that says so.
     formats = {}
@@ -79,47 +79,47 @@ def handle_csv_input(filename, input_specs):
 
     # TODO: Consider making this more modular, instead of using the "dates", "time" and "datetime" as the keys.
 
-    # Check to see if the data/time and/or datetime columns are available and process them
-    # If the formats for the dates and times are specified, use that else try to infer the format.
+    # Check to see if the date/time and/or datetime columns are available and process them
+    # If the formats for the dates and times are specified use them, if not try to infer the format.
     if "date" in header_names:
         if formats_present and "date" in formats:
             date_format = formats["date"]
             try:
-                data["date"] = pd.to_datetime(data["date"], format=date_format)
+                data_frame["date"] = pd.to_datetime(data_frame["date"], format=date_format)
             except Exception as e:
                 print(e)
                 print("Failed to read in date using format {}\n Will try to infer the format".format(date_format))
-                data["date"] = pd.to_datetime(data["date"])
+                data_frame["date"] = pd.to_datetime(data_frame["date"])
         else:
-            data["date"] = pd.to_datetime(data["date"])
+            data_frame["date"] = pd.to_datetime(data_frame["date"])
 
     if "time" in header_names:
         if formats_present and "time" in formats:
             time_format = formats["time"]
             try:
-                data["time"] = pd.to_datetime(data["time"], format=time_format)
+                data_frame["time"] = pd.to_datetime(data_frame["time"], format=time_format)
             except Exception as e:
                 print(e)
                 print("Failed to read in time using format {}\n Will try to infer the format".format(time_format))
-                data["time"] = pd.to_datetime(data["time"])
+                data_frame["time"] = pd.to_datetime(data_frame["time"])
         else:
-            data["time"] = pd.to_datetime(data["time"])
+            data_frame["time"] = pd.to_datetime(data_frame["time"])
 
     if "datetime" in header_names:
         if formats_present and "datetime" in formats:
             datetime_format = formats["datetime"]
             try:
-                data["datetime"] = pd.to_datetime(data["datetime"], format=datetime_format)
+                data_frame["datetime"] = pd.to_datetime(data_frame["datetime"], format=datetime_format)
             except Exception as e:
                 print(e)
                 print("Failed to read in datetime using format {}\n Will try to infer the format"
                       .format(datetime_format))
-                data["datetime"] = pd.to_datetime(data["datetime"])
+                data_frame["datetime"] = pd.to_datetime(data_frame["datetime"])
         else:
-            data["datetime"] = pd.to_datetime(data["datetime"])
+            data_frame["datetime"] = pd.to_datetime(data_frame["datetime"])
     # Change null values to None and then convert the dataframe to a list of dictionaries and return.
-    data = data.where(pd.notnull(data), None)
-    data_list = data.to_dict('records')
+    data_frame = data_frame.where(pd.notnull(data_frame), None)
+    data_list = data_frame.to_dict('records')
     return data_list
 
 
@@ -138,134 +138,199 @@ def handle_xlsx_input(filename, input_specs):
     # Open and read the contents of the json specification file.
     with open(input_specs) as j_maps:
         test_dict = json.load(j_maps)
-    d_frames=None
-    file_header_column = int(test_dict['header_row'])
-    if file_header_column <= NO_HEADER_ROW_INDICATOR:
-        d_frames = pd.read_excel(filename, sheet_name=None, header=None)
-    elif file_header_column == DEFAULT_HEADER_ROW_NUMBER:
-        d_frames = pd.read_excel(filename, sheet_name=None)
-    elif file_header_column > DEFAULT_HEADER_ROW_NUMBER:
-        d_frames = pd.read_excel(filename, sheet_name=None, header=file_header_column-1)
-    rows, cols = d_frames.shape
+
+    # Get the specification for which row contains the headings in the file.
+    d_frame = None  # This will be the dataframe containing the data from 'filename'
+    file_header_row = int(test_dict['header_row'])
+    # Decide how to read in the file based on which row contains the header.
+    if file_header_row <= NO_HEADER_ROW_INDICATOR:
+        d_frame = pd.read_excel(filename, sheet_name=None, header=None)
+    elif file_header_row == DEFAULT_HEADER_ROW_NUMBER:
+        d_frame = pd.read_excel(filename, sheet_name=None)
+    elif file_header_row > DEFAULT_HEADER_ROW_NUMBER:
+        # If the header row is not the first, do this.
+        d_frame = pd.read_excel(filename, sheet_name=None, header=file_header_row-1)
+
+    # Get the number of rows and columns and generate a list containing default header names.
+    rows, cols = d_frame.shape
     header_names = [DEFAULT_UNKNOWN]*cols
 
+    # Change the default header names to the actual header names as specified in the json specification data.
     count = 0
     for item in header_names:
         header_names[count] = item+str(count)
         count += 1
     for key, value in test_dict['mappings'].items():
         value = int(value)
-        pos_val=value-1
+        pos_val = value-1
         if pos_val < len(header_names):
             header_names[pos_val] = key
-    d_frames.columns = header_names
+    d_frame.columns = header_names
 
+    # Check to see if formats are present and generate a boolean value that says so.
     formats_present = False
-    if (test_dict.has_key("formats")):
+    if "formats" in test_dict:
         formats = test_dict["formats"]
-        if (bool(formats)):
+        if bool(formats):
+            # TODO: This boolean value here may be redundant
             formats_present = True
 
-        if ("date" in header_names):
-            type_get = d_frames['date'].apply(type)
+        # TODO: Consider making this more modular, instead of using the "dates", "time" and "datetime" as the keys.
+        if "date" in header_names:
+
+            # Make a data series containing all the types of the data in the 'date' column
+            #  (which is a series in the d_frame) and check to ensure that they are all can all of datetime type.
+            type_get = d_frame['date'].apply(type)
             type_test = (type_get == datetime.datetime).all() or (type_get == datetime.date).all()
 
+            # If the data in the field was not of type datetime, do this.
+            # Try to convert into datetime using the formats specified, if that fails,
+            # Infer the formats of the 'date' field
+            # Use the function pd.to_datetime to convert the fields to dates.
+            # errors='coerce' will force values that cannot be converted correctly to be NaT.
             if not(type_test):
-                if ((formats_present) and formats.has_key("date")):
+                if formats_present and "date" in formats:
                     date_format = formats["date"]
                     try:
-                        d_frames["date"] = pd.to_datetime(d_frames["date"], format=date_format)
+                        d_frame["date"] = pd.to_datetime(d_frame["date"], format=date_format)
                     except Exception as e:
                         print(e)
                         print("Failed to read in date using the format {}\n Will try to infer the format."
                               .format(date_format))
-                        d_frames["date"] = pd.to_datetime(d_frames["date"], errors='coerce')
+                        d_frame["date"] = pd.to_datetime(d_frame["date"], errors='coerce')
                 else:
-                    d_frames["date"] = pd.to_datetime(d_frames["date"], errors='coerce')
+                    d_frame["date"] = pd.to_datetime(d_frame["date"], errors='coerce')
 
-        if ("time" in header_names):
-            type_get = d_frames['time'].apply(type)
+        if "time" in header_names:
+            # Make a data series containing all the types of the data in the 'time' column
+            #  (which is a series in the d_frame) and check to ensure that they are all can all of datetime type.
+            type_get = d_frame['time'].apply(type)
             type_test = (type_get == datetime.datetime).all() or (type_get == datetime.time).all()
 
+            # If the data in the field was not of type datetime, do this.
+            # Try to convert into datetime using the formats specified, if that fails,
+            # Infer the formats of the 'time' field
+            # Use the function pd.to_datetime to convert the values in the field to times.
+            # errors='coerce' will force values that cannot be converted correctly to be NaT.
             if not(type_test):
-                if ((formats_present) and formats.has_key("time")):
+                if formats_present and "time" in formats:
                     time_format = formats["time"]
                     try:
-                        d_frames["time"] = pd.to_datetime(d_frames["time"], format=time_format)
+                        d_frame["time"] = pd.to_datetime(d_frame["time"], format=time_format)
                     except Exception as e:
                         print(e)
                         print("Failed to read in the time using the format {}\n Will try to infer the format."
                               .format(time_format))
-                        d_frames["time"] = pd.to_datetime(d_frames["time"], errors='coerce')
+                        d_frame["time"] = pd.to_datetime(d_frame["time"], errors='coerce')
                 else:
-                    d_frames["time"] = pd.to_datetime(d_frames["time"], errors='coerce')
+                    d_frame["time"] = pd.to_datetime(d_frame["time"], errors='coerce')
 
-        if ("datetime" in header_names):
-            type_get = d_frames["datetime"].apply(type)
+        if "datetime" in header_names:
+            # Make a data series containing all the types of the data in the 'datetime' column
+            #  (which is a series in the d_frame) and check to ensure that they are all can all of datetime type.
+            type_get = d_frame["datetime"].apply(type)
             type_test = (type_get == datetime.datetime).all()
 
+            # If the data in the field was not of type datetime, do this.
+            # Try to convert into datetime using the formats specified, if that fails,
+            # Infer the formats of the 'datetime' field
+            # Use the function pd.to_datetime to convert the values in the field to datetime.
+            # errors='coerce' will force values that cannot be converted correctly to be NaT.
             if not(type_test):
-                if ((formats_present) and formats.has_key("datetime")):
+                if formats_present and "datetime" in formats:
                     datetime_format = formats["datetime"]
                     try:
-                        d_frames["datetime"] = pd.to_datetime(d_frames["datetime"], format=datetime_format)
+                        d_frame["datetime"] = pd.to_datetime(d_frame["datetime"], format=datetime_format)
                     except Exception as e:
                         print(e)
                         print("Failed to read in the datetime using the format {}\n Will try to infer the format."
                               .format(datetime_format))
-                        d_frames["datetime"] = pd.to_datetime(d_frames["datetime"], errors='coerce')
+                        d_frame["datetime"] = pd.to_datetime(d_frame["datetime"], errors='coerce')
                 else:
-                    d_frames["datetime"] = pd.to_datetime(d_frames["datetime"], errors='coerce')
+                    d_frame["datetime"] = pd.to_datetime(d_frame["datetime"], errors='coerce')
 
-    d_frames = d_frames.where(pd.notnull(d_frames), None)
-    data_list = d_frames.to_dict('records')
+    d_frame = d_frame.where(pd.notnull(d_frame), None)
+    data_list = d_frame.to_dict('records')
     return data_list
 
 
 def get_input(filename, input_map):
+    """
+
+    :param filename: The path to the file which will be processed.
+    :param input_map: The path to the json map for the input file that needs to be processed.
+    :return: A list of dictionaries where each dictionary represents a row from the input file being processed.
+
+    This function will process the contents of 'filename'.
+    It will call the functions needed to process '.csv' or '.xlsx' files,
+    and return a list of dictionaries made from the data in the file.
+    """
     print("\nReading input from {}".format(filename))
-    the_path=pathlib2.Path(filename)
+    the_path = pathlib2.Path(filename)
     fname = the_path.name
     filename_split = fname.split('.')
 
-    input_list=[]
+    # Check the type of the file and call the required function to process the file.
+    input_list = []
     if filename_split[-1] == 'csv':
-        input_list=handle_csv_input(filename, input_map)
+        input_list = handle_csv_input(filename, input_map)
     elif filename_split[-1] == 'xls' or filename_split[-1] == 'xlsx':
-        input_list=handle_xlsx_input(filename, input_map)
+        input_list = handle_xlsx_input(filename, input_map)
     else:
         print("Invalid file type, must be '.csv' or '.xlsx' ")
 
     return input_list
 
 
-def process_input(list_of_rows, output_template):
+def format_date_and_time(list_of_rows, output_template):
+    """
+
+    :param list_of_rows: A list of dictionaries where each dictionary represents a row of data.
+    :param output_template: The path to the template file which the data needs to be loaded into.
+    :return: A list of dictionaries with the date/time fields formatted according to the specifications of the user.
+
+    This function will format the date/time fields in the 'list_of_rows' with the formats specified by the user or the
+    default formats used by the data processor. It will also remove any fields that are UNKNOWN.
+    """
     # TODO: Find someway to not use literals when comparing keys
-    print("Processing input gotten from the file...")
-    out_list=[]
+    print("Reformatting date/time fields gotten from the input file...")
+    out_list = []
     with open(output_template) as j_temp_specs:
         template_specs = json.load(j_temp_specs)
     row_template = template_specs["row_template"]
-    count=0
+    count = 0
+
+    # For each dictionary in 'list_of_rows' go through all the fields,
+    # and if there is a date, datetime or time field, reformat it using the format strings specified.
     for dictionary in list_of_rows:
         count += 1
         curr_row = row_template.copy()
         for key, value in dictionary.items():
-            if(not(DEFAULT_UNKNOWN in key)):
+            if not(DEFAULT_UNKNOWN in key):
                 if "date" == key and (value is not pd.NaT) and (value is not None):
-                    value=value.strftime(DEFAULT_DATE_FORMAT)
+                    value = value.strftime(DEFAULT_DATE_FORMAT)
                 elif "time" == key and (value is not pd.NaT) and (value is not None):
-                    value=value.strftime(DEFAULT_TIME_FORMAT)
+                    value = value.strftime(DEFAULT_TIME_FORMAT)
                 elif "datetime" == key and (value is not pd.NaT) and (value is not None):
-                    value=value.strftime(DEFAULT_DATETIME_FORMAT)
+                    value = value.strftime(DEFAULT_DATETIME_FORMAT)
                 curr_row[key] = value
             else:
+                # If the field is still "UNKNOWN" delete it.
                 del dictionary[key]
+        # Add the processed dictionary to the list that will be returned from this funciton.
         out_list.append(curr_row)
     return out_list
 
 
 def handle_xlsx_output(filename, out_list, in_filename, output_specs):
+    """
+
+    :param filename:
+    :param out_list:
+    :param in_filename:
+    :param output_specs:
+    :return:
+    """
     with open(output_specs) as o_specs:
         template_specs = json.load(o_specs)
 
@@ -313,7 +378,7 @@ def transfer_values(inputfile, outputfile, mappings_file, template_json):
        template_json specifications."""
 
     input_rows = get_input(inputfile, mappings_file)
-    output_rows = process_input(input_rows, template_json)
+    output_rows = format_date_and_time(input_rows, template_json)
     saved_path = load_output(output_rows, outputfile, inputfile, template_json)
     return saved_path
 
